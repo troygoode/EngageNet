2b33c6191eea28b0a23d55d0b9eb92416fde3943
Troy Goode <troygoode@gmail.com>
2010-10-29 09:12:25 -0400

    Split packages out into EngageNet and EngageNet-Mvc.

--- packaging/packaging.rb
+++ packaging/packaging.rb
@@ -6,18 +6,44 @@ def copy_files(from, to, filename, extensions)
 	end
 end
 
-task :prepare_package do
-  output_directory_lib = './packaging/EngageNet/lib/40/'
-  FileUtils.mkdir_p output_directory_lib
+task :prepare_package_core do
+  output_directory_core = './packaging/EngageNet/lib/40/'
+  FileUtils.mkdir_p output_directory_core
 
-  copy_files './src/EngageNet/bin/Release/', output_directory_lib, 'EngageNet', ['dll', 'pdb', 'xml']
-  copy_files './src/EngageNet.Mvc/bin/Release/', output_directory_lib, 'EngageNet.Mvc', ['dll', 'pdb', 'xml']
+  copy_files './src/EngageNet/bin/Release/', output_directory_core, 'EngageNet', ['dll', 'pdb', 'xml']
 end
 
-exec :package => :prepare_package do |cmd|
-  cmd.path_to_command = 'packaging/NuPack.exe'
+task :prepare_package_mvc do
+  output_directory_mvc = './packaging/EngageNet-Mvc/lib/40/'
+  FileUtils.mkdir_p output_directory_mvc
+
+  copy_files './src/EngageNet.Mvc/bin/Release/', output_directory_mvc, 'EngageNet.Mvc', ['dll', 'pdb', 'xml']
+end
+
+exec :package_core => :prepare_package_core do |cmd|
+  cmd.log_level = :verbose
+  cmd.path_to_command = 'packaging/NuPack-CTP2.exe'
   cmd.parameters [
+  	'pack',
     'packaging\\EngageNet\\EngageNet.nuspec',
-    'packaging\\EngageNet'
+    '-o packaging\\EngageNet'
   ]
+end
+
+exec :package_mvc => :prepare_package_mvc do |cmd|
+  cmd.path_to_command = 'packaging/NuPack-CTP2.exe'
+  cmd.parameters [
+  	'pack',
+    'packaging\\EngageNet-Mvc\\EngageNet-Mvc.nuspec',
+    '-o packaging\\EngageNet-Mvc'
+  ]
+end
+
+task :package => [:package_core, :package_mvc] do
+end
+
+task :clean_packages do
+	FileUtils.rm './packaging/EngageNet/lib/'
+	FileUtils.rm './packaging/EngageNet/EngageNet.1.0.0'
+	FileUtils.rm './packaging/EngageNet-Mvc/lib/'
 end
