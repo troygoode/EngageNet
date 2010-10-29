512d5791605020185fd65457da59cece654c5c3e
Troy Goode <troygoode@gmail.com>
2010-10-29 11:47:37 -0400

    Renamed EngageNet-Mvc package to EngageNet.Mvc

--- packaging/packaging.rb
+++ packaging/packaging.rb
@@ -14,9 +14,9 @@ task :prepare_package_core => :release do
 end
 
 task :prepare_package_mvc => :release do
-  output_directory_mvc_lib = './packaging/EngageNet-Mvc/lib/40/'
-  output_directory_mvc_content_controllers = './packaging/EngageNet-Mvc/content/Controllers/'
-  output_directory_mvc_content_views = './packaging/EngageNet-Mvc/content/Views/Engage/'
+  output_directory_mvc_lib = './packaging/EngageNet.Mvc/lib/40/'
+  output_directory_mvc_content_controllers = './packaging/EngageNet.Mvc/content/Controllers/'
+  output_directory_mvc_content_views = './packaging/EngageNet.Mvc/content/Views/Engage/'
   FileUtils.mkdir_p output_directory_mvc_lib
   FileUtils.mkdir_p output_directory_mvc_content_controllers
   FileUtils.mkdir_p output_directory_mvc_content_views
@@ -48,8 +48,8 @@ exec :package_mvc => :prepare_package_mvc do |cmd|
   cmd.path_to_command = 'packaging/NuPack-CTP2.exe'
   cmd.parameters [
   	'pack',
-    'packaging\\EngageNet-Mvc\\EngageNet-Mvc.nuspec',
-    '-o packaging\\EngageNet-Mvc'
+    'packaging\\EngageNet.Mvc\\EngageNet.Mvc.nuspec',
+    '-o packaging\\EngageNet.Mvc'
   ]
 end
 
@@ -60,7 +60,7 @@ task :clean_packages do
 	FileUtils.rm_r './packaging/EngageNet/lib/' unless not File.directory? './packaging/EngageNet/lib/'
 	FileUtils.rm Dir.glob './packaging/EngageNet/*.nupkg'
 
-	FileUtils.rm_r './packaging/EngageNet-Mvc/lib/' unless not File.directory? './packaging/EngageNet-Mvc/lib/'
-	FileUtils.rm_r './packaging/EngageNet-Mvc/content/' unless not File.directory? './packaging/EngageNet-Mvc/content/'
-	FileUtils.rm Dir.glob './packaging/EngageNet-Mvc/*.nupkg'
+	FileUtils.rm_r './packaging/EngageNet.Mvc/lib/' unless not File.directory? './packaging/EngageNet.Mvc/lib/'
+	FileUtils.rm_r './packaging/EngageNet.Mvc/content/' unless not File.directory? './packaging/EngageNet.Mvc/content/'
+	FileUtils.rm Dir.glob './packaging/EngageNet.Mvc/*.nupkg'
 end
